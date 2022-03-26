9280280239ec8a5eda42a7d883470fbf33370027
Andy Phu <orangebookair@gmail.com>
2022-03-26 15:22:50 -0500

    ddddd

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from asyncio import streams
+from os import rename
 from pytube import YouTube
 import json
 from mutagen.mp3 import MP3
@@ -27,24 +28,33 @@ def wipeMetadata(fname):
     file = MP3(fname)
     file.delete()
     file.save()
+    print("wiped "+ fname)
 
 def download(links):
     for link in links:
         vid = YouTube(link)
-        recentlyDownloaded.append([vid.title,link])
+        print("downloading " + vid.title)
         soundStreams = vid.streams.filter(only_audio=True)
         firstStream = str(soundStreams[0]).split(" ") #Get first stream
         itag = firstStream[1][6:-1]#Get itag value
         vid.streams.get_by_itag(itag).download()
-
-
+        recentlyDownloaded.append(vid.title+".mp4")
+        print("downloaded " + vid.title)
 
 if(settings["JSONInput"]==True):
     download(getLinkTxt(linksR))
 elif(settings["JSONInput"]==False):
     download(getLinkConsole())
 
-
+i=0
+for fname in recentlyDownloaded:
+    #f = open(fname)
+    print("renaming " + fname)
+    newName = fname[0:-4]+".mp3"
+    rename(fname,newName)
+    wipeMetadata(newName)
+    recentlyDownloaded[i] = newName
+    i+=1
 #if(settings["WipeMetadata"]==True):
 #    for audioFiles in recentlyDownloaded:
 #        wipeMetadata(audioFiles[0])
